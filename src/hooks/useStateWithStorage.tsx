f02b29f0415c84d041a52e38fdbd614320ba706a
Virginia Cepeda <virginiamcepeda@gmail.com>
2024-03-06 14:48:02 -0300

    Implement useStateWithStorage hook

--- src/hooks/useStateWithStorage.tsx
+++ src/hooks/useStateWithStorage.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function useStateWithStorage(
   key: string,
   defaultValue: unknown
-) {}
+) {
+  // Fetch initial state from localStorage or use default value
+  const [storedValue, setStoredValue] = useState(() => {
+    const storedItem = localStorage.getItem(key);
+    return storedItem ? JSON.parse(storedItem) : defaultValue;
+  });
+
+  // Update localStorage whenever state changes
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [key, storedValue]);
+
+  // Return state and a function to update state
+  return [storedValue, setStoredValue];
+}
